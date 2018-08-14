7bb5b4b17cd803b5386fbd35b10029c546411223
crookfion <crookfion@gs1608.AD.ECS.VUW.AC.NZ>
2018-08-14 14:44:11 +1200

    AnimatedBugworld - start button fixed
    Now the start button will properly reset the world, as well as the clear
    (prev reset) button

--- FC_AnimatedBugWorld/src/AnimationUI.java
+++ FC_AnimatedBugWorld/src/AnimationUI.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 import javafx.animation.KeyFrame;
 import javafx.animation.TimelineBuilder;
 import javafx.application.Application;
@@ -7,7 +5,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -40,9 +37,12 @@ public class AnimationUI extends Application {
 	
 	TextField numBugs=new TextField();
 	TextField numPlants=new TextField();
+	boolean running =false;
+	
 	
 	@SuppressWarnings("deprecation")
 	@Override
+	
 	public void start(Stage primaryStage) throws Exception {
 		pane.setCenter(panel);
 
@@ -57,49 +57,36 @@ public class AnimationUI extends Application {
 		startBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent arg0) { //what we want to happen when button pressed
-								
-				findStartNumbers();				
-				startWorld.populate();
-				
-				//put plants into group individually
-				int plantListSize=startWorld.getPlantListSize();
-				for(int i=0;i<plantListSize;i++) {
-					panel.getChildren().add(startWorld.getPlantList().get(i));
-					//group.getChildren().add(startWorld.getPlantList().get(i).getCircle());
+				if(!running) {
+					startBugWorld();
+					running=true;
+					
+				} else {
+					clearBugWorld();
+					startBugWorld();
+					running=true;
+					
 				}
-				
-				//put bugs into group individually
-				int bugListSize=startWorld.getBugListSize();
-				for(int i=0;i<bugListSize;i++) {
-					panel.getChildren().add(startWorld.getBugList().get(i));
-					//group.getChildren().add(startWorld.getBugList().get(i).getCircle());
-				}		
 			}
 			
 		}); //end startBtn set action
 		
+		
 		//create a reset button
 		Button resetBtn=new Button();
-		resetBtn.setText("Reset");
+		resetBtn.setText("Clear");
 		
 		resetBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent arg0) { //what we want to happen when button pressed
-				//remove plants from group individually				
-				for(int i=0;i<startWorld.getPlantList().size();i++) {
-					panel.getChildren().remove(startWorld.getPlantList().get(i));
-				}
 				
-				//remove bugs from group individually
-				for(int i=0; i<startWorld.getBugList().size();i++) {
-					panel.getChildren().remove(startWorld.getBugList().get(i));
-				}	
+				clearBugWorld();
+				running=false;
 				
-				//clear out world values
-				startWorld.restart();
 			}
 			
 		}); //end resettBtn set action
+		
 		
 		//what to do at each keyframe
 		KeyFrame frame =new KeyFrame(Duration.millis(16),new EventHandler<ActionEvent>() {
@@ -121,17 +108,18 @@ public class AnimationUI extends Application {
 		Label title=new Label("Welcome to Bug World");
 		title.setAlignment(Pos.CENTER);
 		BackgroundFill backFill=new BackgroundFill(Color.WHITE,null, null);
-		title.setBackground(new Background(backFill));
+		//title.setBackground(new Background(backFill));
 		title.setFont(Font.font ("Verdana", 20));
 		title.setPadding(new Insets(15));
 		
-		
+		//create labels for the text fields
 		Label numBugLabel=new Label("Number of Bugs (max 50)");
 		Label numPlantLabel=new Label("Number of Plants (max 50)");
 		
-		
+		//create border around bug movement window
 		panel.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 		
+		//set background to white
 		pane.setBackground(new Background(backFill));
 		
 		//menu for buttons down the left side
@@ -140,6 +128,7 @@ public class AnimationUI extends Application {
 		leftside.setPadding(new Insets(10));
 		leftside.setSpacing(10);
 		
+		//add things to borderpane
 		pane.setTop(title);
 		pane.setLeft(leftside);
 
@@ -150,6 +139,39 @@ public class AnimationUI extends Application {
 
 	} //end start
 	
+	public void startBugWorld() {
+		findStartNumbers();				
+		startWorld.populate();
+		
+		//put plants into group individually
+		int plantListSize=startWorld.getPlantListSize();
+		for(int i=0;i<plantListSize;i++) {
+			panel.getChildren().add(startWorld.getPlantList().get(i));
+		}
+		
+		//put bugs into group individually
+		int bugListSize=startWorld.getBugListSize();
+		for(int i=0;i<bugListSize;i++) {
+			panel.getChildren().add(startWorld.getBugList().get(i));
+		}	
+	}
+	
+	public void clearBugWorld() {
+		//remove plants from group individually				
+		for(int i=0;i<startWorld.getPlantList().size();i++) {
+			panel.getChildren().remove(startWorld.getPlantList().get(i));
+		}
+		
+		//remove bugs from group individually
+		for(int i=0; i<startWorld.getBugList().size();i++) {
+			panel.getChildren().remove(startWorld.getBugList().get(i));
+		}	
+		
+		//clear out world values
+		startWorld.restart();
+	}
+	
+	//the updates to occur each keyFrame
 	public void updateBugWorld() {
 		
 		startWorld.updateWorldSize(panel.getWidth(), panel.getHeight());
@@ -203,7 +225,7 @@ public class AnimationUI extends Application {
 		if(num.isEmpty()) {
 			return false;
 		}
-		//convert string to individual chars, if any char is not a number
+		//convert string to individual chars, checks if any char is not a number
 		//also prevents negative numbers
 		for(int i=0;i<num.length();i++) {
 			char c = num.charAt(i);
@@ -211,7 +233,7 @@ public class AnimationUI extends Application {
 	            return false;
 	        }
 		}
-			return true;	
+		return true;	
 	}
 
 	public static void main(String[] args) {
